2727e4da3aa6920d0d944c09b464602b756ef09f
matrixguy69 <blouchsaab587@gmail.com>
2026-02-23 23:17:47 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Wizmn Hub');
+function toggleTheme(){document.body.classList.toggle('light')}
+function checkInvite(){
+const c=document.getElementById('code').value;
+document.getElementById('msg').textContent=
+c==='WIZMN-FOUNDING'?'Request received.':'Invalid code.';
+}
+function savePost(){
+const t=document.getElementById('anonText').value;
+localStorage.setItem('wizmn_post',t);
+alert('Saved locally.');
+}
